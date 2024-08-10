3fd26d71ed33ba72c75ae7cd0c65eaed48e1aea4
Bethan Palmer <bpalmer@toogoodtogo.com>
2024-08-10 12:59:44 +0200

    Add end state for 03_02

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,8 +1,34 @@
 package com.example;
 
+import java.lang.foreign.Arena;
+import java.lang.foreign.FunctionDescriptor;
+import java.lang.foreign.Linker;
+
+import static java.lang.foreign.ValueLayout.ADDRESS;
+import static java.lang.foreign.ValueLayout.JAVA_LONG;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Throwable {
+        try (final var arena = Arena.ofConfined()) {
+
+            final var linker = Linker.nativeLinker();
+
+            final var symbolLookup = linker.defaultLookup();
+
+            final var memorySegment = symbolLookup.find("strlen").orElseThrow();
+
+            final var functionDescriptor =
+                    FunctionDescriptor.of(JAVA_LONG, ADDRESS);
+
+            final var methodHandle = linker.downcallHandle(memorySegment, functionDescriptor);
+
+            final var segmentAllocator = arena.allocateFrom("Hello world");
+
+            final var result = (long) methodHandle.invokeExact(segmentAllocator);
+
+            System.out.println(result);
+        }
 
     }
 
