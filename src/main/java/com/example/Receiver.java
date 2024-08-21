aab14c5bb354cc07122d7693a62d12e84777d345
Bethan Palmer <bpalmer@toogoodtogo.com>
2024-08-21 19:35:15 +0200

    Add 02_04

--- src/main/java/com/example/Receiver.java
+++ src/main/java/com/example/Receiver.java
@@ -1,9 +1,17 @@
 package com.example;
 
+import javax.crypto.DecapsulateException;
+import javax.crypto.KEM;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
 public class Receiver {
 
-    byte[] getKey(byte[] encapsulation, java.security.PrivateKey privateKey) {
-        return new byte[0];
+    byte[] getKey(KEM.Encapsulated encapsulated, java.security.PrivateKey privateKey) throws NoSuchAlgorithmException, InvalidKeyException, DecapsulateException {
+        final var receiversKem = KEM.getInstance("DHKEM");
+        final var receiver = receiversKem.newDecapsulator(privateKey);
+        final var receivedSecretKey = receiver.decapsulate(encapsulated.encapsulation());
+        return receivedSecretKey.getEncoded();
     }
 
 
